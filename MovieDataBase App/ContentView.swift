3502133f38bb1a79ee3367c07d5a4611d2483fc3
Dmitrii Proshutinskii <koptehe@mail.ru>
2021-03-18 18:27:16 +0300

    Initial Commit

---
+++ MovieDataBase App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieDataBase App
+//
+//  Created by Физтех.Радио on 18.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
